25f43b90061d380687a97f7bc442c8b4bc41e7e4
Manav <manav@manav.tailc9cf2e.ts.net>
2026-06-25 21:02:50 +0530

    03 learning c some orograms and learning

--- Practice/03_c_basic.c
+++ Practice/03_c_basic.c
@@ -16,7 +16,7 @@
 
 // calculating the gross salery of Ramesh 
  
-int main() {
+/* int main() {
     int basic_salary, da, ra, gross_salary;
 
     printf("Enter the Ramesh's basic salary:");
@@ -30,5 +30,47 @@ int main() {
 
     
     return 0;
+} */
+
+//convert distance from km to meter, cm, feetand inch 
+
+/* int main() {
+
+    float km, m, cm, in, ft;
+    printf("Enter distance in km: ");
+    scanf("%f", &km);
+    m = km * 1000;
+    cm = km * 100000;
+    in = km * 39370.1;
+    ft = km * 3280.84;
+
+    printf("Distance between two cties in meter: %f\n", m);
+    printf("Distance between two cties in cm: %f\n", cm);
+    printf("Distance between two cties in inch: %f\n", in);
+    printf("Distance between two cties in feet: %f\n", ft);
+
+    return 0;
+
+} */
+
+//find out the aggrigate marks and percentage markes obtained by a student in 5 subjects.
+
+int  main() {
+
+    int sub1, sub2, sub3, sub4, sub5;
+    float total_marks, percentage;
+
+    printf("Enter marks of 5 subject of a student: ");
+    scanf("%d %d %d %d %d", &sub1, &sub2, &sub3, &sub4, &sub5);
+
+    total_marks = sub1 + sub2 + sub3 + sub4 + sub5;
+
+    percentage = total_marks / 5;
+
+    printf("Total aggrigate marks obtained by a student in 5 subject is: %f\n", total_marks);
+    printf("Percentage marks obtained by a student in 5 subject is: %f\n", percentage);
+
+    return 0;
+    
 }
- 
+  
